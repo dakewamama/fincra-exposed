1d84a00badb2cde196d9d341374ed34715ed3d64
dakewamama <davidasiadiachi@gmail.com>
2025-11-19 15:48:29 +0000

    remove dist from git tracking

--- fincra-payout/dist/fincra/fincra.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { FincraService } from './fincra.service';
-import { BusinessResponse } from './interfaces/business.interface';
-export declare class FincraController {
-    private readonly fincraService;
-    constructor(fincraService: FincraService);
-    getBusinessId(): Promise<BusinessResponse>;
-}
--- fincra-payout/dist/fincra/fincra.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { BusinessResponse } from './interfaces/business.interface';
-export declare class FincraService {
-    private config;
-    private readonly client;
-    constructor(config: ConfigService);
-    private handleError;
-    getBusinessId(): Promise<BusinessResponse>;
-}
